92b986e60d4fd4f280ddf8e909725e563bf16b0e
Seth VanHeulen <svanheulen@structart.com>
2010-12-28 19:35:22 -0500

    added documentation and license

--- main.c
+++ main.c
@@ -1,3 +1,10 @@
+/*
+
+  Copyright (c) 2010, Seth VanHeulen
+  All rights reserved.
+
+ */
+
 #include <pspkernel.h>
 #include <pspdebug.h>
 #include <pspumd.h>
@@ -9,20 +16,27 @@ PSP_MODULE_INFO("MonsterHunterSaveConverter", PSP_MODULE_KERNEL, 0, 1);
 PSP_NO_CREATE_MAIN_THREAD();
 
 struct game_info_t {
+    // memory offset where the character data starts
     int offset;
+    // name of the main module of the game
     char module_name[28];
+    // length of the character data in memory
     int size;
+    // name of the file to save/load character data to/from
     char file_name[50];
+    // indicates if the file given in file_name exists
     int file_exists;
 };
 
 int control_module_threads(char* module_name, int paused) {
+    // find the game's module id
     SceUID module_id;
     SceModule* module_info = sceKernelFindModuleByName(module_name);
     if (module_info)
         module_id = module_info->modid;
     else
         return 0;
+    // get an array of all threads
     int thread_count;
     if (sceKernelGetThreadmanIdList(SCE_KERNEL_TMID_Thread, NULL, 0, &thread_count) < 0)
         return 0;
@@ -35,10 +49,12 @@ int control_module_threads(char* module_name, int paused) {
     SceKernelThreadInfo thread_info;
     thread_info.size = sizeof(SceKernelThreadInfo);
     for (i = 0; i < thread_count; i++) {
+        // determine if a thread is from the game
         if (sceKernelReferThreadStatus(thread_id[i], &thread_info))
             continue;
         module_info = sceKernelFindModuleByAddress((unsigned int) thread_info.entry);
         if (module_info && module_info->modid == module_id) {
+            // pause or resume the game's threads
             if (paused)
                 sceKernelResumeThread(thread_id[i]);
             else
@@ -49,9 +65,11 @@ int control_module_threads(char* module_name, int paused) {
 }
 
 int check_game(struct game_info_t* game_info) {
+    // actiave the umd
     sceUmdCheckMedium();
     sceUmdActivate(1, "disc0:");
     sceUmdWaitDriveStat(UMD_WAITFORINIT);
+    // read the game's id from the umd
     SceUID umd_data = sceIoOpen("disc0:/UMD_DATA.BIN", PSP_O_RDONLY, 0777);
     if (umd_data < 0)
         return 0;
@@ -62,6 +80,7 @@ int check_game(struct game_info_t* game_info) {
     }
     sceIoClose(umd_data);
     game_id[10] = 0;
+    // set the correct offset for each game id
     if (strcmp("ULJM-05500", game_id) == 0)
         game_info->offset = 0x1195e30;
     else if (strcmp("ULES-01213", game_id) == 0)
@@ -70,6 +89,7 @@ int check_game(struct game_info_t* game_info) {
         game_info->offset = 0x119a230;
     else
         return 0;
+    // read the game's module name from the umd
     SceUID eboot = sceIoOpen("disc0:/PSP_GAME/SYSDIR/EBOOT.BIN", PSP_O_RDONLY, 0777);
     if (eboot < 0)
         return 0;
@@ -79,12 +99,14 @@ int check_game(struct game_info_t* game_info) {
         return 0;
     }
     sceIoClose(eboot);
+    // set the correct size and file name for each module name
     if (strcmp("MonsterHunterPortable2ndG", game_info->module_name) == 0) {
         game_info->size = 0x6a880;
         strcat(game_info->file_name, "mhp2g.bin");
     } else {
         return 0;
     }
+    // check if a save file already exists
     SceIoStat stat;
     if (sceIoGetstat(game_info->file_name, &stat) < 0)
         game_info->file_exists = 0;
@@ -94,23 +116,28 @@ int check_game(struct game_info_t* game_info) {
 }
 
 int save(struct game_info_t* game_info) {
+    // open or create the save file
     SceUID file = sceIoOpen(game_info->file_name, PSP_O_WRONLY | PSP_O_CREAT | PSP_O_TRUNC, 0777);
     if (file < 0)
         return 0;
+    // read character data from memory and write it into the save file
     char* mem_pointer = (char*) (0x8800000 + game_info->offset);
     if (sceIoWrite(file, mem_pointer, game_info->size) < game_info->size) {
         sceIoClose(file);
         return 0;
     }
     sceIoClose(file);
+    // mark the save file as existing
     game_info->file_exists = 1;
     return 1;
 }
 
 int load(struct game_info_t* game_info) {
+    // open an existing save file
     SceUID file = sceIoOpen(game_info->file_name, PSP_O_RDONLY, 0777);
     if (file < 0)
         return 0;
+    // read character data from the save file and write it to memory
     char* mem_pointer = (char*) (0x8800000 + game_info->offset);
     if (sceIoRead(file, mem_pointer, game_info->size) < game_info->size) {
         sceIoClose(file);
@@ -121,13 +148,16 @@ int load(struct game_info_t* game_info) {
 }
 
 int delete(struct game_info_t* game_info) {
+    // delete an existing save file
     if (sceIoRemove(game_info->file_name) < 0)
         return 0;
+    // mark the save file as not existing
     game_info->file_exists = 0;
     return 1;
 }
 
 void display_message(struct game_info_t* game_info, const char* message, unsigned int color) {
+    // print some info and controls to the screen
     pspDebugScreenSetTextColor(0x00ffffff);
     pspDebugScreenSetXY(0, 0);
     pspDebugScreenKprintf("game: ");
@@ -148,28 +178,34 @@ void display_message(struct game_info_t* game_info, const char* message, unsigne
 
 int main_thread(SceSize argc, void* argp) {
     struct game_info_t game_info;
+    // get the path that the plugin was loaded from
     int path_length = strrchr((char*) argp, '/') - (char*) argp + 1;
     strncpy(game_info.file_name, argp, path_length);
     game_info.file_name[path_length] = 0;
+    // check that the loaded game is supported
     if (!check_game(&game_info)) {
         sceKernelExitDeleteThread(0);
         return 0;
     }
+    // setup the controller
     sceCtrlSetSamplingCycle(0);
     sceCtrlSetSamplingMode(PSP_CTRL_MODE_DIGITAL);
     SceCtrlData pad;
+    // wait for input
     int paused = 0;
     while (1) {
         sceCtrlPeekBufferPositive(&pad, 1);
         if (pad.Buttons != 0) {
             if (paused) {
                 if (pad.Buttons & PSP_CTRL_CIRCLE) {
+                    // resume the game
                     if (control_module_threads(game_info.module_name, 1))
                         paused = 0;
                     else
                         display_message(&game_info, "resume failed.  ", 0x000000ff);
                 }
                 if (pad.Buttons & PSP_CTRL_SQUARE) {
+                    // save character
                     display_message(&game_info, "saving...       ", 0x00ff0000);
                     if (save(&game_info))
                         display_message(&game_info, "save complete.  ", 0x0000ff00);
@@ -177,6 +213,7 @@ int main_thread(SceSize argc, void* argp) {
                         display_message(&game_info, "save failed.    ", 0x000000ff);
                 }
                 if (pad.Buttons & PSP_CTRL_CROSS && game_info.file_exists) {
+                    // load character
                     display_message(&game_info, "loading...      ", 0x00ff0000);
                     if (load(&game_info))
                         display_message(&game_info, "load complete.  ", 0x0000ff00);
@@ -184,6 +221,7 @@ int main_thread(SceSize argc, void* argp) {
                         display_message(&game_info, "load failed.    ", 0x000000ff);
                 }
                 if (pad.Buttons & PSP_CTRL_TRIANGLE && game_info.file_exists) {
+                    // delete save file
                     display_message(&game_info, "deleting...     ", 0x00ff0000);
                     if (delete(&game_info))
                         display_message(&game_info, "delete complete.", 0x0000ff00);
@@ -192,6 +230,7 @@ int main_thread(SceSize argc, void* argp) {
                 }
             } else {
                 if (pad.Buttons & PSP_CTRL_NOTE) {
+                    // pause the game
                     if (control_module_threads(game_info.module_name, 0)) {
                         paused = 1;
                         pspDebugScreenInit();
@@ -207,6 +246,7 @@ int main_thread(SceSize argc, void* argp) {
 }
 
 int module_start(SceSize argc, void* argp) {
+    // create plugin main thread
     SceUID thid = sceKernelCreateThread("mhsc_main", main_thread, 0x30, 0x10000, 0, NULL);
     if (thid >= 0)
         sceKernelStartThread(thid, argc, argp);
